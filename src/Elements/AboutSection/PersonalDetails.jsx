b4870f902a7590127fc43a01e3bc107dab5e921f
LuffyDrago <rigelvicky@gmail.com>
2022-02-01 23:01:29 +0300

    Adapting Animations

--- src/Elements/AboutSection/PersonalDetails.jsx
+++ src/Elements/AboutSection/PersonalDetails.jsx
@@ -1,17 +1,27 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
 import * as styles from "./PersonalDetailStyle";
 import { Link } from "react-router-dom";
 const PersonalDetails = () => {
+  AOS.init({
+    duration: 2000, // values from 0 to 3000, with step 50ms
+    easing: "ease", // default easing for AOS animations
+  });
   return (
     <>
       <styles.PersonalSection>
         <styles.Container>
-          <div className="heroleft">
+          <div className="heroleft" data-aos="fade-right">
             <styles.Heroheading>
               <p>Meet Victor</p>
             </styles.Heroheading>
           </div>
           <div className="heroright">
-            <div className="HeroRightText">
+            <div
+              className="HeroRightText"
+              data-aos="fade-left"
+              data-aos-duration="1500"
+            >
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
               expedita tempora harum qui cupiditate ullam dolores libero dolorem
               sapiente odit?
@@ -19,6 +29,7 @@ const PersonalDetails = () => {
           </div>
         </styles.Container>
         <img
+          data-aos="zoom-in-up"
           src="https://uploads-ssl.webflow.com/5f58a59d36085d27ba519f2f/61d2f3d51ddecd681d8e11ee_Mask%20Group.jpg"
           alt="picture1"
         />
@@ -26,7 +37,7 @@ const PersonalDetails = () => {
           <div className="twosides">
             <div className="firstside"></div>
             <div className="lastside">
-              <div className="lastsidetext">
+              <div className="lastsidetext" data-aos="fade-left">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem
                 similique impedit reprehenderit ab nemo temporibus, tenetur
                 autem ducimus. Consequatur, eligendi?
@@ -39,7 +50,7 @@ const PersonalDetails = () => {
           <div className="twosides">
             <div className="firstside"></div>
             <div className="lastside">
-              <div className="lastsidetext">
+              <div className="lastsidetext" data-aos="fade-right">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Eveniet distinctio voluptatem officiis suscipit molestias nisi,
                 vitae eligendi praesentium. Placeat, pariatur?
@@ -52,7 +63,7 @@ const PersonalDetails = () => {
           <div className="twosides">
             <div className="firstside"></div>
             <div className="lastside">
-              <div className="lastsidetext">
+              <div className="lastsidetext" data-aos="fade-left">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius,
                 rerum nobis! Cumque iure reiciendis nemo sit itaque praesentium
                 veritatis eligendi.
